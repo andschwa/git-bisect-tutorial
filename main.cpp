7ffb3cc5f0a4772f1e145969c30e84824a7637f2
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2019-07-06 16:21:31 -0700

    Fixed a typo

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(int argc, char** argv)
 {
   cout << "I think things are good!" << endl;
-  cout << "More change" << endl;
+  cout << "More changes" << endl;
   return 1;
 }
